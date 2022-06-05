9e2611b476a7dfedacf4402911dc96b9c5d677c2
Oleh Brusentsov <Oleh_Brusentsov@epam.com>
2022-06-05 23:12:56 +0300

    Minimum API applied

--- Mentoring/Program.cs
+++ Mentoring/Program.cs
@@ -1,44 +1,28 @@
 using Mentoring.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Logging.AddLog4Net();
+    builder.Services.AddDbContext<NorthwindContext>(options => options.UseSqlServer(
+        builder.Configuration.GetSection("ConnectionStrings").Value));
 
-// TODO: Minimum api (not related to any of tasks)
-namespace Mentoring
-{
-    public class Program
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
     {
-        
-        private static readonly log4net.ILog _log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-        public static void Main(string[] args)
-        {
-            _log.Info("Starting application");
-            var builder = WebApplication.CreateBuilder(args);
-            _log.Info("Builder created");
-            builder.Logging.AddLog4Net();
-            builder.Services.AddDbContext<NorthwindContext>(options => options.UseSqlServer(
-                builder.Configuration.GetSection("ConnectionStrings").Value));
-
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-
-            app.Run();
-        }
+        app.UseExceptionHandler("/Home/Error");
     }
-}
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
